6160d58b83f2a8bccee6f4f4cff2833e2efe7b38
kelalm <34616059+kelalm@users.noreply.github.com>
2023-03-19 03:41:13 -0400

    Added a container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,32 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Container } from "@mantine/core";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
   return (
     <>
-      <Head>
-        <title>Page title</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
       <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
         theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
+          components: {
+            Container: {
+              defaultProps: {
+                sizes: {
+                  xs: 540,
+                  sm: 720,
+                  md: 960,
+                  lg: 1140,
+                  xl: 1320,
+                },
+              },
+            },
+          },
         }}
       >
-        <Component {...pageProps} />
+        <Container size="sm">
+          <Component {...pageProps} />
+        </Container>
       </MantineProvider>
     </>
   );
