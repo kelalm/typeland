a920eee3b595d9b88d4d1c8f5bc8cfba771f6460
kelalm <34616059+kelalm@users.noreply.github.com>
2023-03-27 02:41:42 -0400

    changed Textbox to a textarea
    fetching sample game text from baconipsum api

--- components/GameWindow.tsx
+++ components/GameWindow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextBox from "./TextBox";
 import Timer from "./Timer";
 import WPMBox from "./WPMBox";
@@ -6,6 +6,20 @@ import TimeSelector from "./TimeSelector";
 
 const GameWindow: React.FC = () => {
   const [typingStarted, setTypingStarted] = useState(false);
+  const [gameText, setGameText] = useState("");
+
+  useEffect(() => {
+    // Fetch text for TextBox
+    // Using sample text for now
+    fetch("https://baconipsum.com/api/?paras=1&type=all-meat")
+      .then((response) => response.json())
+      .then((data) => {
+        setGameText(data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const handleTypingStarted = () => {
     setTypingStarted(true);
@@ -13,8 +27,8 @@ const GameWindow: React.FC = () => {
 
   return (
     <div>
-      <TextBox onTypingStarted={handleTypingStarted} />
       {!typingStarted && <TimeSelector />}
+      <TextBox onTypingStarted={handleTypingStarted} gameText={gameText} />
       {typingStarted && <Timer />}
       {typingStarted && <WPMBox />}
     </div>
--- components/TextBox.tsx
+++ components/TextBox.tsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Textarea } from "@mantine/core";
 
 type TextBoxProps = {
   onTypingStarted: () => void;
+  gameText: string;
 };
 
-const TextBox: React.FC<TextBoxProps> = ({ onTypingStarted }) => {
+const TextBox: React.FC<TextBoxProps> = ({ onTypingStarted, gameText }) => {
   const handleTyping = () => {
     onTypingStarted();
   };
 
-  return <input type="text" onChange={handleTyping} />;
+  return (
+    <>
+      <Textarea
+        placeholder={gameText}
+        // label="Your comment"
+        radius="lg"
+        size="xl"
+        withAsterisk
+        onChange={handleTyping}
+      />
+    </>
+  );
 };
 
 export default TextBox;
